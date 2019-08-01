ff3e69c919cd76a09bfe1bca880853ded747e483
Filipe Alves <52730205+filipe30@users.noreply.github.com>
2019-08-01 14:54:06 -0300

    Modificado

--- main_doc.py
+++ main_doc.py
@@ -8,7 +8,7 @@ from subprocess import call
 
 hj = date.today()
 dias = ('Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo')
-mes = {1:'janeiro', 2:'fevereiro', 3:'março',  4:'abril', 5:'maio', 6:'junho', 7:'julho', 8:'agosto', 9:'setembro', 10:'outubro', 11:'novembro', 12:'desembro'}
+mes = {1:'janeiro', 2:'fevereiro', 3:'março',  4:'abril', 5:'maio', 6:'junho', 7:'julho', 8:'agosto', 9:'setembro', 10:'outubro', 11:'novembro', 12:'dezembro'}
 janela = Tk()
 dia_la = Label(janela,text=(dias[hj.weekday()]+','+ ' '+ str(hj.day)+' '+'de'+' '+str(mes[hj.month])+' '+'de'+' '+str(hj.year)), font='Helvita 50 bold', fg='blue')
 dia_la.place(x=200, y=750)
